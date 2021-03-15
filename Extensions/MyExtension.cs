63108f470d1473dc6a7fab703873d0e4cffd00b0
iebv <iebvidea@gmail.com>
2021-03-15 10:36:51 -0500

    Initial commit

---
+++ Extensions/MyExtension.cs
@@ -0,0 +1,59 @@
+﻿using CloudNative.CloudEvents;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCloudEventsNuget
+{
+    class MyExtension : ICloudEventExtension
+    {
+
+        private const string CustomAttributeName = "custom";
+
+        private string _custom;
+        public string Custom
+        {
+            get => _custom;
+            set
+            {
+                _custom = value;
+                AllAttributes.Add(CustomAttributeName, _custom);
+            }
+        }
+
+        private Dictionary<string, object> AllAttributes { get; }
+
+        public MyExtension()
+        {
+            AllAttributes = new Dictionary<string, object>();
+        }
+        
+
+        //This methos must be implmented for adding the attribute(s) to the CloudEvent instance
+        public void Attach(CloudEvent cloudEvent)
+        {
+            foreach(var key in AllAttributes.Keys)
+                cloudEvent.GetAttributes().Add(key, AllAttributes[key]);
+        }
+
+        public Type GetAttributeType(string name)
+        {
+            AllAttributes.TryGetValue(name, out object value);
+            return value.GetType();
+        }
+
+        public bool ValidateAndNormalize(string key, ref dynamic value)
+        {
+            if (AllAttributes.ContainsKey(key))
+            {
+                if (AllAttributes.TryGetValue(key, out object val))
+                    return val == value;
+            }
+            return false;
+               
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using CloudNative.CloudEvents;
+using CloudNative.CloudEvents.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace DemoCloudEventsNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Built in extension
+            var ext = new DistributedTracingExtension()
+            {
+                TraceParent = "value"
+            };
+
+            //Custom extension
+            var myExt = new MyExtension()
+            {
+                Custom = "value"
+            };
+
+            ICloudEventExtension[] extensions = { ext, myExt };
+
+            //Constructor
+            var cloudEvent = new CloudEvent(
+                CloudEventsSpecVersion.V1_0, //Specification
+                "test-type", //Type
+                new Uri("https://github.com/cloudevents/spec/pull/123"), //Source
+                "test-subject", //Subject
+                Guid.NewGuid().ToString(), //Id
+                DateTime.Now, //time
+                extensions
+            )
+            { 
+                DataContentType = new ContentType("application/json"),
+                Data = new { Name = "Ivan Ballesteros", Company = "S4N", Age = 29}
+            };
+
+            //Accessing custom extension via method
+            Console.WriteLine($"The value of the custom attr is: {cloudEvent.Extension<MyExtension>().Custom}");
+
+            //Get content
+            var content = new CloudEventContent(cloudEvent,
+                                     ContentMode.Structured,
+                                     new JsonEventFormatter());
+
+            //Print result as JSON
+            Console.WriteLine(content.ReadAsStringAsync().Result);
+            
+
+        }
+    }
+}
